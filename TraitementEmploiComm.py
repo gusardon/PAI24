5c9421b452d1e2406a8ee5e27d5245d44e768020
gusardon <36200559+gusardon@users.noreply.github.com>
2018-02-06 18:13:27 +0100

    Update TraitementEmploiComm.py

--- TraitementEmploiComm.py
+++ TraitementEmploiComm.py
@@ -8,7 +8,20 @@ Created on Tue Dec 19 17:23:15 2017
 import xlrd
 
 #nomFichier = 'EFF_APE_AUVERGNE_RHONE_ALPES.xls'
+
+"""Le fichier 'EFF_APE_AUVERGNE_RHONE_ALPES.xls' a la forme suivante :
+Il s'agit d'un excel avec les feuilles correspoondant aux départements de la région Auvergne - Rhône Alpes. Ex : 01, 03, 07, 15, 26, 38, 42, 43...
+Une feuille a la structure suivante : ( commune (numéro + nom) ; domaine d'activité ; nombre d'établissements 2008 ; ... ; nombre d'établissements 2016 ; effectif disponible 2008 ; ... ; effectif disponible 2016 )
+Chaque ligne correspond à un secteur d'activité dans une commune donnée. on passe donc en revu tous les secteurs d'activité par commune."""
+
+
+"""Cette fontion a pour but de construire un tableau qui regroupe les numéro insee des communes et le nombre d'emplois disponibles dans cette communes 
+quels que soient les secteurs d'activité. Pour cela on commence par charger les données d'emploi de l'institut Acoss puis on ne garde que les données 
+du département iDep, enfin, on somme les nombres d'emplois disponibles suivant les secteurs d'activités.
+On renvoit ainsi un tableau de la forme ( ID_commune ; nb_emplois ) propres au département iDep"""
+
 #iDep = '69'
+
 def loadXlsEmplois(nomFichier, iDep):
     donnees_brutes = xlrd.open_workbook(nomFichier)
     commune69 = donnees_brutes.sheet_by_name(iDep)
